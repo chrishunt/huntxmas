d47c4ea96617e148fe4b9cba79c5c1770f5135ec
Chris Hunt <huntca@gmail.com>
2011-11-25 10:54:05 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xmaslist::Application.config.secret_token = 'f99839a3c6c5330e8d354f159e48e0c228b80daeaeb0c8880ebf8c845559c2bff1af031edc5916c8e8dec922359d6fa48e9e2a1c75692c5e4145b362165c6b84'
