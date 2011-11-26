b10269998b9e0c38f8783ff43697bd559824dd9d
Chris Hunt <huntca@gmail.com>
2011-11-25 22:16:48 -0800

    Add db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,22 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20111126035750) do
+
+  create_table "gifts", :force => true do |t|
+    t.string   "name"
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
