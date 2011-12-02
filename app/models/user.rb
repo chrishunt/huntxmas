4da717064c38caadeff48acc1d64b9b70ac13a6d
Chris Hunt <huntca@gmail.com>
2011-12-01 22:34:09 -0800

    Add user#purchase to mark gifts as purchased

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ActiveRecord::Base
   validates :name,  :presence => true
   validates :email, :presence => true, :uniqueness => true
   validates :password, :presence => { :on => :create }
+
+  def purchase(gift)
+    gift.update_attributes(:purchased_by_user_id => self.id) unless gift.user == self
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,40 +1,81 @@
 require 'spec_helper.rb'
 
 describe User do
-  before(:each) do
-    @user = Factory.build(:user)
+  describe 'validations' do
+    before(:each) do
+      @user = Factory.build(:user)
+    end
+
+    it 'saves when all validations are met' do
+      @user.save.should == true
+      saved_user = User.first
+      saved_user.name.should == @user.name
+      saved_user.email.should == @user.email
+    end
+
+    it 'requires presence of name' do
+      @user.name = nil
+      @user.save.should == false
+    end
+
+    it 'requires presence of email' do
+      @user.email = nil
+      @user.save.should == false
+    end
+
+    it 'requires unique email address' do
+      user1 = Factory.build(:user, :email => 'user@email.com')
+      user1.save.should == true
+      user2 = Factory.build(:user, :email => 'user@email.com')
+      user2.save.should == false
+    end
+
+    it 'requires presence of password' do
+      @user.password = nil
+      @user.save.should == false
+    end
+
+    it 'has many gifts' do
+      @user.gifts.should_not == nil
+    end
   end
 
-  it 'saves when all validations are met' do
-    @user.save.should == true
-    saved_user = User.first
-    saved_user.name.should == @user.name
-    saved_user.email.should == @user.email
-  end
+  describe '#purchase' do
+    before(:each) do
+      @user = Factory(:user)
+    end
 
-  it 'requires presence of name' do
-    @user.name = nil
-    @user.save.should == false
-  end
+    context 'when gift is owned by another user' do
+      before(:each) do
+        @another_user = Factory(:user)
+        @gift = Factory(:gift, :user => @another_user)
+        @user.purchase(@gift)
+        @gift = Gift.find(@gift)
+      end
 
-  it 'requires presence of email' do
-    @user.email = nil
-    @user.save.should == false
-  end
+      it 'marks the gift as purchased' do
+        @gift.purchased?.should == true
+      end
 
-  it 'requires unique email address' do
-    user1 = Factory.build(:user, :email => 'user@email.com')
-    user1.save.should == true
-    user2 = Factory.build(:user, :email => 'user@email.com')
-    user2.save.should == false
-  end
+      it 'assigns the correct user id to the purchased gift' do
+        @gift.purchased_by_user_id.should == @user.id
+      end
+    end
 
-  it 'requires presence of password' do
-    @user.password = nil
-    @user.save.should == false
-  end
+    context 'when gift is owned by the same user' do
+      before(:each) do
+        @gift = Factory(:gift, :user => @user)
+        @user.purchase(@gift)
+        @gift = Gift.find(@gift)
+      end
 
-  it 'has many gifts' do
-    @user.gifts.should_not == nil
+      it 'does not mark the gift as purchased' do
+        @gift.purchased?.should == false
+      end
+
+      it 'does not assign a user id to the gift' do
+        @gift.purchased_by_user_id.should == nil
+      end
+    end
   end
 end
