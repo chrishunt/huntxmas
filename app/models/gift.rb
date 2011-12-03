2801acff79ecfbb971ce945b69ac17836bc2cfea
Chris Hunt <huntca@gmail.com>
2011-12-02 21:49:33 -0800

    Add sell!, return!, purchased_by?, and purchaser to gift.rb

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -5,7 +5,27 @@ class Gift < ActiveRecord::Base
   validates :name, :presence => true
   validates :url,  :presence => true
 
+  def sell!(user)
+    self.update_attributes(:purchased_by_user_id => user.id) unless self.user == user
+  end
+
+  def return!
+    self.update_attributes(:purchased_by_user_id => nil)
+  end
+
   def purchased?
     !self.purchased_by_user_id.nil?
+  end
+
+  def purchased_by?(user)
+    self.purchased_by_user_id == user.id
+  end
+
+  def purchaser
+    begin
+      User.find(self.purchased_by_user_id)
+    rescue ActiveRecord::RecordNotFound
+      nil
+    end
   end
 end
--- spec/models/gift_spec.rb
+++ spec/models/gift_spec.rb
@@ -1,34 +1,71 @@
 require 'spec_helper.rb'
 
 describe Gift do
-  before(:each) do
-    @gift = Factory.build(:gift)
+  describe 'validations' do
+    before(:each) do
+      @gift = Factory.build(:gift)
+    end
+
+    it 'saves a gift when all validations are passing' do
+      @gift.save.should == true
+      saved_gift = Gift.first
+      saved_gift.name.should == @gift.name
+      saved_gift.url.should  == @gift.url
+      saved_gift.user.should == @gift.user
+    end
+
+    it 'validates presence of name' do
+      @gift.name = nil
+      @gift.save.should == false
+    end
+
+    it 'validates presence of url' do
+      @gift.url = nil
+      @gift.save.should == false
+    end
+
+    it 'validates precense of user' do
+      @gift.user = nil
+      @gift.save.should == false
+    end
   end
 
-  it 'saves a gift when all validations are passing' do
-    @gift.save.should == true
-    saved_gift = Gift.first
-    saved_gift.name.should == @gift.name
-    saved_gift.url.should  == @gift.url
-    saved_gift.user.should == @gift.user
-  end
+  describe '#sell!' do
+    before(:each) do
+      @creator      = Factory(:user)
+      @another_user = Factory(:user)
+    end
 
-  it 'validates presence of name' do
-    @gift.name = nil
-    @gift.save.should == false
-  end
+    context 'when purchased by another user' do
+      before(:each) do
+        gift = Factory(:gift, :user => @creator)
+        gift.sell!(@another_user)
+        @gift = Gift.find(gift)
+      end
 
-  it 'validates presence of url' do
-    @gift.url = nil
-    @gift.save.should == false
-  end
+      it 'sets the perchased_by_user_id to the correct user' do
+        @gift.purchased_by_user_id.should == @another_user.id
+      end
+    end
 
-  it 'validates precense of user' do
-    @gift.user = nil
-    @gift.save.should == false
+    context 'when purchased by the gift creater' do
+      before(:each) do
+        gift = Factory(:gift, :user => @creator)
+        gift.sell!(@creator)
+        @gift = Gift.find(gift)
+      end
+
+      it 'does not set the purchased_by_user_id' do
+        @gift.purchased_by_user_id.should == nil
+      end
+    end
   end
 
   describe '#purchased?' do
+    before(:each) do
+      @gift = Factory(:gift)
+    end
+
     it 'returns true when the gift has been purchased' do
       @gift.stub!(:purchased_by_user_id).and_return(1)
       @gift.purchased?.should == true
@@ -37,6 +74,55 @@ describe Gift do
     it 'returns false when the gift has not been purchased' do
       @gift.stub!(:purchased_by_user_id).and_return(nil)
       @gift.purchased?.should == false
+    end
+  end
+
+  describe '#purchased_by?' do
+    before(:each) do
+      @gift = Factory(:gift)
+      @user = Factory(:user)
+      @user.purchase(@gift)
+    end
+
+    it 'returns true if the gift was purchased by the user' do
+      @gift.purchased_by?(@user).should == true
+    end
+
+    it 'returns false if the gift was not purchased by the user' do
+      @gift.purchased_by?(Factory(:user)).should == false
+    end
+  end
+
+  describe '#return!' do
+    before(:each) do
+      gift = Factory(:gift, :purchased_by_user_id => 1)
+      gift.return!
+      @gift = Gift.find(gift)
+    end
+
+    it 'returns the gift to an unpurchased state' do
+      @gift.purchased?.should == false
+    end
+  end
+
+  describe '#purchaser' do
+    before(:each) do
+      @gift = Factory(:gift)
+    end
+
+    context 'when gift is purchased' do
+      before(:each) do
+        @user = Factory(:user)
+        @user.purchase(@gift)
+      end
+
+      it 'returns the user that purchased the gift' do
+        @gift.purchaser.should == @user
+      end
+    end
+
+    it 'returns nil when gift is not purchased' do
+      @gift.purchaser.should == nil
     end
   end
 end
