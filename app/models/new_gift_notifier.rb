27eea5f5eb2059bd332dd650547e5ab46c367ef7
Chris Hunt <c@squareup.com>
2012-12-26 20:40:53 -0800

    Don't notify of their own new gifts

--- app/models/new_gift_notifier.rb
+++ app/models/new_gift_notifier.rb
@@ -1,6 +1,8 @@
 class NewGiftNotifier
   def notifications_for(user, gift)
-    User.with_email_notifications.each do |to|
+    User.with_email_notifications.where(
+      "id IS NOT ?", user.id
+    ).each do |to|
       email = GiftMailer.gift_added_notification(user, to, gift)
       email.deliver if email
     end
--- spec/models/new_gift_notifier_spec.rb
+++ spec/models/new_gift_notifier_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 describe NewGiftNotifier do
   describe '#notifications_for' do
     it 'sends an email to all users with notifications enabled' do
+      user = FactoryGirl.create(:user)
+
       2.times do
         FactoryGirl.create(:user) do |user|
           user.email_notifications = true
@@ -14,7 +16,17 @@ describe NewGiftNotifier do
 
       GiftMailer.should_receive(:gift_added_notification).twice
 
-      subject.notifications_for(nil, nil)
+      subject.notifications_for user, nil
+    end
+
+    it 'does not notify users of their own gifts' do
+      user = FactoryGirl.create(:user)
+      user.email_notifications = true
+      user.save
+
+      GiftMailer.should_not_receive(:gift_added_notification)
+
+      subject.notifications_for user, nil
     end
   end
 end
