3e9f342cff2783a2de6b9af712d5789ebe3d5d90
Chris Hunt <c@squareup.com>
2012-12-26 17:34:12 -0800

    Add email template for gift added notification

--- app/mailers/gift_mailer.rb
+++ app/mailers/gift_mailer.rb
@@ -2,6 +2,10 @@ class GiftMailer < ActionMailer::Base
   default from: 'no-reply@huntxmas.com'
 
   def gift_added_notification(from, to, gift)
+    @from = from
+    @to = to
+    @gift = gift
+
     mail(
       to: to.email,
       subject: "#{from.name} added a new Christmas gift"
