6e5eca74720ce00f3666d705ef1605169b47ab2c
Chris Hunt <chrishunt@github.com>
2013-12-06 09:54:58 -0800

    Use Postgres compatible 'not equal' in notifier

--- app/models/new_gift_notifier.rb
+++ app/models/new_gift_notifier.rb
@@ -1,7 +1,7 @@
 class NewGiftNotifier
   def notifications_for(user, gift)
     User.with_email_notifications.where(
-      "id IS NOT ?", user.id
+      "id <> ?", user.id
     ).each do |to|
       email = GiftMailer.gift_added_notification(user, to, gift)
       email.deliver if email
