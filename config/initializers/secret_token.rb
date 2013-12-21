7b1df93bbc0be7b8843486e2afeeec7f1dc43080
Chris Hunt <chrishunt@github.com>
2013-12-21 10:05:58 -0800

    Add secret_key_base to secret_token
    Needed for Rails 4.0

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Xmaslist::Application.config.secret_token = 'f99839a3c6c5330e8d354f159e48e0c228b80daeaeb0c8880ebf8c845559c2bff1af031edc5916c8e8dec922359d6fa48e9e2a1c75692c5e4145b362165c6b84'
+Xmaslist::Application.config.secret_key_base = '60c108ca5bb6ad15a642b42e5860f9942c544658159fa54249eb3fabd32eac4884dfaa0eb3e7825c823a60fe32f38ec667c14d5c1f3bf1b60fd14de2e313e19e'
